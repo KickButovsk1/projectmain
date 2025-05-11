c7e0e4beb511aa47d2a1d3d3c6557409b8c65341
kickbutovk1 <uupeedxd@mail.ru>
2025-05-11 15:09:20 +0500

    second commit

---
+++ NagruzkaClg/View/EditGroupPage.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace NagruzkaClg.View;
+
+public partial class EditGroupPage : Page
+{
+    public EditGroupPage()
+    {
+        InitializeComponent();
+    }
+}
